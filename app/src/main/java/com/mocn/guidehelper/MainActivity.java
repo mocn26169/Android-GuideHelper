11d5fe860dc49f218452195ecd15694d518a806b
mocn2619 <mocn26169@qq.com>
2017-02-20 17:35:25 +0800

    second

--- app/src/main/java/com/mocn/guidehelper/MainActivity.java
+++ app/src/main/java/com/mocn/guidehelper/MainActivity.java
@@ -1,7 +1,10 @@
 package com.mocn.guidehelper;
 
+import android.app.Dialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.PopupWindow;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(this, "this is a tip", Toast.LENGTH_SHORT).show();
+        Dialog dialog;
+        PopupWindow popWindow;
     }
 }
